26e9444bda4146127c7649daada5d16d063b2fed
Titouan <tpoquillon@gmail.com>
2019-05-02 11:04:09 +0200

    Ecriture de la methode affichage pour Valeur
    renvoi "True" si val_==True, "Fals" sinon
    
    Signed-off-by: Titouan <tpoquillon@gmail.com>

--- Valeur.cpp
+++ Valeur.cpp
@@ -4,5 +4,13 @@ Valeur::Valeur(bool b):val_(b){
 }
 
 bool Valeur::Calcule(bool* b) const{
+
 return val_;
+}
+
+std::string Valeur::Affiche(bool* b) const{
+
+return  (val_) ? std::string("True") : std::string("False");
+
+
 }
