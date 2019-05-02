6bc8eb3f143a0d33cf2030c4b1368f9f68a00cc7
Titouan <tpoquillon@gmail.com>
2019-05-02 11:01:17 +0200

    Ajout d'une methode Affichage dans Noeud
    Signed-off-by: Titouan <tpoquillon@gmail.com>

--- Noeud.cpp
+++ Noeud.cpp
@@ -1,1 +1,6 @@
 #include"Noeud.h"
+
+std::string Noeud::Affiche(bool*) const{
+
+return std::string("True");
+}
--- Noeud.h
+++ Noeud.h
@@ -1,8 +1,12 @@
 #ifndef NODE_ 
 #define NODE_
+#include <string>
+
 class Noeud{
 
 public:
 virtual bool Calcule(bool*) const=0;
+
+virtual std::string Affiche(bool*) const;
 };
 #endif //NODE_
